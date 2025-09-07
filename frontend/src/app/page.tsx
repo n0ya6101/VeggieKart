459baa7a98a6173b029b43c99eee6132a59936e6
Noya <142074263+n0ya6101@users.noreply.github.com>
2025-09-08 01:22:22 +0530

    corrected api for fetching products

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import type { Product } from '@/app/types';
 
 async function getProducts(): Promise<Product[]> {
   try {
-    const response = await fetch('http://localhost:5001/api/products', { cache: 'no-store' });
+    const response = await fetch('https://vegiekart-api.onrender.com/api/products', { cache: 'no-store' });
     if (!response.ok) throw new Error('Network response was not ok');
     return response.json();
   } catch (error) {
